eb7bef40e27a3d8d392ab0221abf9ad9840c4e31
Nhi Nguyen <nnguy152@gmail.com>
2018-03-31 18:14:02 -0400

    make table for preferences

--- src/preferences/Preferences.js
+++ src/preferences/Preferences.js
@@ -1,31 +1,50 @@
 import React, { Component } from 'react';
+import './Preferences.css'
 
 class Preferences extends Component {
   render() {
     return (
-      <div>
+      <div className="table-container">
         <h1>Consumables</h1>
-        <table style="width:100%">
-          <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <td>Jill</td>
-            <td>Smith</td>
-            <td>50</td>
-          </tr>
-          <tr>
-            <td>Eve</td>
-            <td>Jackson</td>
-            <td>94</td>
-          </tr>
-          <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>80</td>
-          </tr>
+        <table>
+          <tbody>
+            <th>Food</th>
+            <tr>
+              <td><label><input className="btn" type="checkbox" />Dairy</label></td>
+              <td><label><input className="btn" type="checkbox" />Protein</label></td>
+              <td><label><input className="btn" type="checkbox" />Bread</label></td>
+              <td><label><input className="btn" type="checkbox" />Pastry</label></td>
+            </tr>
+            <tr>
+              <td><label><input className="btn" type="checkbox" />Fruits</label></td>
+              <td><label><input className="btn" type="checkbox" />Vegetables</label></td>
+              <td><label><input className="btn" type="checkbox" />Snacks</label></td>
+              <td><label><input className="btn" type="checkbox" />Baby Food</label></td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table>
+          <tbody>
+            <th>Drinks</th>
+            <tr>
+              <td><label><input type="radio" />Water</label></td>
+              <td><label><input type="radio" />Other</label></td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table>
+          <tbody>
+            <th>Other</th>
+            <tr>
+              <td><label><input type="radio" />Low Sodium</label></td>
+              <td><label><input type="radio" />Gluten Free</label></td>
+              <td><label><input type="radio" />Baking Staples</label></td>
+              <td><label><input type="radio" />Pet Food</label></td>
+              <td><label><input type="radio" />Other</label></td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
