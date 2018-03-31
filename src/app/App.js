d587ea1b8a97060711ae436cb36e263d46129176
Ai Yen Tam <tam.aiyen@gmail.com>
2018-03-31 18:25:14 -0400

    extra app.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Signup from "../signup/Signup";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
 import "../app/App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Signup />
+        <Footer />
       </div>
     );
   }
