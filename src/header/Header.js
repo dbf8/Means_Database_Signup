a492c02c92e8a01c13940ed6f3d1987feda56ef3
Nhi Nguyen <nnguy152@gmail.com>
2018-03-31 15:44:22 -0400

    make header & footer

---
+++ src/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img className="logo" src="https://meansdatabase.herokuapp.com/assets/means_logo_header-2b31c16501dcaaa15595be933a5a966b336cd9ecf7fb2907fe3143732986de69.png" />
+        </div>
+    );
+}
+
+export default Header;
